d93d1324052893216551a3803985ea1e66866c54
joeri <joeri.kaiser@hotmail.com>
2025-06-18 19:18:03 +0200

    set up cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,12 +1,33 @@
 package middleware
 
 import (
+	"os"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 )
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
+		allowedOrigins := os.Getenv("CORS_ALLOWED_ORIGINS")
+
+		origins := strings.Split(allowedOrigins, ",")
+
+		requestOrigin := c.GetHeader("Origin")
+
+		var allowedOrigin string
+		for _, origin := range origins {
+			trimmedOrigin := strings.TrimSpace(origin)
+			if trimmedOrigin == requestOrigin {
+				allowedOrigin = requestOrigin
+				break
+			}
+		}
+
+		if allowedOrigin != "" {
+			c.Header("Access-Control-Allow-Origin", allowedOrigin)
+		}
+
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		c.Header("Access-Control-Expose-Headers", "Content-Length")
